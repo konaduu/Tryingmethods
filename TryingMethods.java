d822bcab6ef85d8c2175add914238cb426c4e538
konaduu <konadu@Konadus-MacBook-Pro.local>
2022-10-19 21:24:38 +0000

    dynamic objects created

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,7 +1,11 @@
 // This class contains two dynamic and two static methods 
 public class TryingMethods {
     public static void main(String[] args) {
-     
+        TryingMethods dynamicmethods = new TryingMethods();
+        System.out.println(dynamicmethods.myDynamicOne(6));
+        dynamicmethods.myDynamicTwo();
+        myStaticOne();
+        myStaticTwo();
 
     }
 
