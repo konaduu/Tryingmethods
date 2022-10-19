20bb7a7855ead7210f7bb73a1a8b892b4ad30567
konaduu <konadu@Konadus-MacBook-Pro.local>
2022-10-19 20:47:01 +0000

    created my first methods

---
+++ TryingMethods.java
@@ -0,0 +1,27 @@
+// This class contains two dynamic and two static methods 
+public class TryingMethods {
+    public static void main(String[] args) {
+     
+
+    }
+
+    static int myStaticOne() {
+
+    };
+
+    static void myStaticTwo() {
+   
+    }
+
+    // dynamic methods
+    String myDynamicOne() {
+   
+
+    };
+
+    void myDynamicTwo() {
+
+
+
+    }
+}
